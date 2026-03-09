35b6696df221e52daf28bbf0f3ddf6465318384b
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-03-09 20:03:52 +0000

    update

--- src/edit.js
+++ src/edit.js
@@ -1,20 +1,28 @@
-import { useBlockProps, MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
-import { Button, TextControl } from '@wordpress/components';
+import { useBlockProps, MediaUpload, MediaUploadCheck, InspectorControls } from '@wordpress/block-editor';
+import { Button, TextControl, PanelBody, SelectControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 export default function Edit({ attributes, setAttributes }) {
-    const { files, headerText } = attributes;
+    const { files, headerText, layoutStyle } = attributes;
 
     const onSelectFiles = (mediaItems) => {
         const newFiles = mediaItems.map(media => ({
             id: media.id,
             url: media.url,
-            realFileName: media.filename, // The actual filename on the server
-            displayName: media.title,    // The name you want users to see
+            realFileName: media.filename,
+            displayName: media.title,
             fileSize: media.filesizeHumanReadable,
             fileLabel: media.url.split('.').pop().toUpperCase().substring(0, 4)
         }));
         setAttributes({ files: [...files, ...newFiles] });
+    };
+
+    const moveFile = (oldIndex, newIndex) => {
+        if (newIndex < 0 || newIndex >= files.length) return;
+        const updatedFiles = [...files];
+        const [movedItem] = updatedFiles.splice(oldIndex, 1);
+        updatedFiles.splice(newIndex, 0, movedItem);
+        setAttributes({ files: updatedFiles });
     };
 
     const updateFileTitle = (newName, index) => {
@@ -23,45 +31,59 @@ export default function Edit({ attributes, setAttributes }) {
         setAttributes({ files: updatedFiles });
     };
 
-    const removeFile = (index) => {
-        setAttributes({ files: files.filter((_, i) => i !== index) });
-    };
-
     return (
-        <div {...useBlockProps({ className: 'mdb-editor-container' })}>
-            <TextControl
-                label={__('Block Header', 'mdb')}
-                value={headerText}
-                onChange={(val) => setAttributes({ headerText: val })}
-            />
+        <>
+            <InspectorControls>
+                <PanelBody title={__('Layout Settings', 'mdb')}>
+                    <SelectControl
+                        label={__('Display Style', 'mdb')}
+                        value={layoutStyle}
+                        options={[
+                            { label: __('Standard Row', 'mdb'), value: 'row' },
+                            { label: __('Compact List', 'mdb'), value: 'compact' },
+                        ]}
+                        onChange={(val) => setAttributes({ layoutStyle: val })}
+                    />
+                </PanelBody>
+            </InspectorControls>
 
-            <div className="mdb-files-list">
-                {files.map((file, index) => (
-                    <div key={index} className="mdb-download-row mdb-editor-row">
-                        <div className="mdb-file-type-icon">{file.fileLabel}</div>
-                        <div className="mdb-download-details">
-                            <TextControl
-                                value={file.displayName}
-                                onChange={(val) => updateFileTitle(val, index)}
-                                help={`Source: ${file.realFileName} (${file.fileSize})`}
-                            />
-                        </div>
-                        <Button isDestructive onClick={() => removeFile(index)}>×</Button>
-                    </div>
-                ))}
-            </div>
-
-            <MediaUploadCheck>
-                <MediaUpload
-                    onSelect={onSelectFiles}
-                    multiple={true}
-                    render={({ open }) => (
-                        <Button variant="secondary" onClick={open}>
-                            {__('Add Files', 'mdb')}
-                        </Button>
-                    )}
+            <div {...useBlockProps({ className: `mdb-editor-wrapper mdb-style-${layoutStyle}` })}>
+                <TextControl
+                    label={__('Block Header', 'mdb')}
+                    value={headerText}
+                    onChange={(val) => setAttributes({ headerText: val })}
                 />
-            </MediaUploadCheck>
-        </div>
+
+                <div className="mdb-files-list">
+                    {files.map((file, index) => (
+                        <div key={index} className="mdb-download-row mdb-editor-row">
+                            <div className="mdb-editor-controls">
+                                <Button icon="arrow-up-alt2" onClick={() => moveFile(index, index - 1)} disabled={index === 0} />
+                                <Button icon="arrow-down-alt2" onClick={() => moveFile(index, index + 1)} disabled={index === files.length - 1} />
+                            </div>
+                            <div className="mdb-file-type-icon">{file.fileLabel}</div>
+                            <div className="mdb-download-details">
+                                <TextControl
+                                    value={file.displayName}
+                                    onChange={(val) => updateFileTitle(val, index)}
+                                    help={`${__('Source:', 'mdb')} ${file.realFileName} (${file.fileSize})`}
+                                />
+                            </div>
+                            <Button isDestructive icon="trash" onClick={() => setAttributes({ files: files.filter((_, i) => i !== index) })} />
+                        </div>
+                    ))}
+                </div>
+
+                <MediaUploadCheck>
+                    <MediaUpload
+                        onSelect={onSelectFiles}
+                        multiple={true}
+                        render={({ open }) => (
+                            <Button variant="secondary" onClick={open} className="mdb-add-btn">{__('Add Files', 'mdb')}</Button>
+                        )}
+                    />
+                </MediaUploadCheck>
+            </div>
+        </>
     );
 }
--- src/save.js
+++ src/save.js
@@ -1,25 +1,24 @@
 import { useBlockProps } from '@wordpress/block-editor';
 
 export default function save({ attributes }) {
-    const { files, headerText } = attributes;
-
+    const { files, headerText, layoutStyle } = attributes;
     if (!files || files.length === 0) return null;
 
     return (
-        <div {...useBlockProps.save({ className: 'mdb-download-container' })}>
-            <h3 className="mdb-download-header">{headerText}</h3>
-            {files.map((file, index) => (
-                <div key={index} className="mdb-download-row">
-                    <div className="mdb-file-type-icon">{file.fileLabel}</div>
-                    <div className="mdb-download-details">
-                        <div className="mdb-download-title">{file.displayName}</div>
-                        <div className="mdb-download-size">{file.fileSize}</div>
+        <div {...useBlockProps.save({ className: `mdb-container mdb-style-${layoutStyle}` })}>
+            {headerText && <h3 className="mdb-front-header">{headerText}</h3>}
+            <div className="mdb-list-wrapper">
+                {files.map((file, index) => (
+                    <div key={file.id || index} className="mdb-download-row">
+                        <div className="mdb-file-type-icon">{file.fileLabel}</div>
+                        <div className="mdb-download-details">
+                            <div className="mdb-download-title">{file.displayName}</div>
+                            <div className="mdb-download-size">{file.fileSize}</div>
+                        </div>
+                        <a href={file.url} className="mdb-download-button" download>{'DOWNLOAD'}</a>
                     </div>
-                    <a href={file.url} className="mdb-download-button" download>
-                        DOWNLOAD
-                    </a>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
