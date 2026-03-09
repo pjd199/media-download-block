c331d1bc5e73ef8e38ac73622ff53967ed720c2e
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-03-09 16:59:03 +0000

    Update media-download-block.php

--- media-download-block.php
+++ media-download-block.php
@@ -1,39 +1,36 @@
 <?php
 /**
  * Plugin Name: Media Download Block
- * Description: A clean file download block for the Gutenberg editor.
- * Version: 0.0.1
- * Author: Pete Dibdin
- * GitHub Plugin URI: https://github.com/pjd199/media-download-block
- * License: MIT
+ * Version: 1.2.0
+ * Author: WPDM
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function media_download_block_init() {
-    if ( ! file_exists( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' ) ) {
-        return;
-    }
+function wpdm_media_download_block_init() {
+    $asset_path = plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
+    if ( ! file_exists( $asset_path ) ) return;
 
-    $asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php' );
+    $asset_file = include( $asset_path );
 
+    // Standardized Block Name: wpdm/media-download
     register_block_type( 'wpdm/media-download', array(
-        'editor_script' => 'media-download-block-js',
-        'style'         => 'media-download-block-css',
+        'editor_script' => 'wpdm-block-js',
+        'style'         => 'wpdm-block-css',
     ) );
 
     wp_register_script(
-        'media-download-block-js',
+        'wpdm-block-js',
         plugins_url( 'build/index.js', __FILE__ ),
         $asset_file['dependencies'],
         $asset_file['version']
     );
 
     wp_register_style(
-        'media-download-block-css',
-        plugins_url( 'build/style-index.css', __FILE__ ),
+        'wpdm-block-css',
+        plugins_url( 'build/index.css', __FILE__ ),
         array(),
         $asset_file['version']
     );
 }
-add_action( 'init', 'media_download_block_init' );
+add_action( 'init', 'wpdm_media_download_block_init' );
