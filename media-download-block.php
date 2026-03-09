befcdbf1b146fe624664d081910e10e23695ca8f
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-03-09 19:31:32 +0000

    update

--- media-download-block.php
+++ media-download-block.php
@@ -8,34 +8,10 @@
  * License: MIT
  * 
  */
-
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 function mdb_register_block_init() {
-    $asset_path = plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
-    if ( ! file_exists( $asset_path ) ) return;
-
-    $asset_file = include( $asset_path );
-
-    // Block handle: mdb/media-download
-    register_block_type( 'mdb/media-download', array(
-        'editor_script' => 'mdb-block-js',
-        'editor_style'  => 'mdb-block-editor-css',
-        'style'         => 'mdb-block-css',
-    ) );
-
-    wp_register_script(
-        'mdb-block-js',
-        plugins_url( 'build/index.js', __FILE__ ),
-        $asset_file['dependencies'],
-        $asset_file['version']
-    );
-
-    wp_register_style(
-        'mdb-block-css',
-        plugins_url( 'build/style-index.css', __FILE__ ),
-        array(),
-        $asset_file['version']
-    );
+    // Points to the folder where the build version of block.json lives
+    register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'mdb_register_block_init' );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
+import './style.scss'; // This stays to tell Webpack to compile the CSS
 import Edit from './edit';
 import save from './save';
+import metadata from './block.json'; // Import the JSON
 
-registerBlockType( 'wpdm/media-download', {
-    title: 'Media Download', // This is what shows in the block inserter
-    icon: 'download',
-    category: 'common',
-    attributes: {
-        mediaID: { type: 'number' },
-        mediaURL: { type: 'string' },
-        mediaTitle: { type: 'string' },
-        mediaSize: { type: 'string' },
-        fileLabel: { type: 'string', default: 'FILE' },
-        customName: { type: 'string', default: '' }
-    },
+registerBlockType( metadata.name, {
+    ...metadata, // Spreads all attributes/settings from block.json
     edit: Edit,
     save,
 });
