cc6affa0d3e03f77f797ad98741d77603bc8187f
pjd199 <89210268+pjd199@users.noreply.github.com>
2026-03-10 19:40:42 +0000

    test

--- src/edit.js
+++ src/edit.js
@@ -160,10 +160,10 @@ export default function Edit({ attributes, setAttributes }) {
                 {files.length > 0 ? (
                     <div className={`mdb-container mdb-style-${layoutStyle}`}>
                         <p>TEST!!!</p>
-                        <!--<ServerSideRender
+                        {/*<ServerSideRender
                             block="mdb/media-download"
                             attributes={ attributes }
-                        />-->
+                        />*/}
                     </div>
                 ) : (
                     <div style={{ 
